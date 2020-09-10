a8341c0a93972d1b68b056f52ecc3c24422196f1
unknown <hasinimanage@gmail.com>
2020-09-10 07:15:47 +0530

    changing styles

--- dom&events.js
+++ dom&events.js
@@ -16,3 +16,11 @@
 
   </body>
 </html>
+
+//**changing styles */
+<div id="demo" style="width:200px">some text</div>
+<script>
+  var x = document.getElementById("demo");
+  x.style.color = "6600FF";
+  x.style.width = "100px";
+</script>
