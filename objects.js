8dc9e69c521da19fda70991ce9e50320389bdd98
unknown <hasinimanage@gmail.com>
2020-09-10 06:45:01 +0530

    Objet initializing

--- objects.js
+++ objects.js
@@ -12,3 +12,16 @@ var p2 = new person("Amy", 21, "red");
 document.write(p1.age); // Outputs 42
 document.write(p2.name); // Outputs "Amy"
 
+
+//**onject initializing */
+var John = {
+    name: "John",
+    age: 25
+  };
+  var James = {
+    name: "James",
+    age: 21
+  };
+
+  document.write(John.age);
+//Outputs 25
