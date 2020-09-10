bc8b9399a66c4718b3610f6930fa3f9439efb3e9
unknown <hasinimanage@gmail.com>
2020-09-10 06:46:59 +0530

    Adding methods

--- objects.js
+++ objects.js
@@ -23,5 +23,18 @@ var John = {
     age: 21
   };
 
-  document.write(John.age);
-//Outputs 25
+  document.write(John.age); //Outputs 25
+
+
+//**adding methods */
+
+function person(name, age) {
+    this.name = name;  
+    this.age = age;
+    this.changeName = function (name) {
+      this.name = name;
+    }
+  }
+  
+  var p = new person("David", 21);
+  p.changeName("John");  //Now p.name equals to "John"
