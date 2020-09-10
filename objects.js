7b228473293b83f29d83fb8fefa37a82fa2758d7
unknown <hasinimanage@gmail.com>
2020-09-10 06:42:55 +0530

    Creating an object

--- objects.js
+++ objects.js
@@ -3,3 +3,12 @@ var person = {
     name: "John", age: 31, 
     favColor: "green", height: 183
    };
+
+
+//**Creatinf an object */
+var p1 = new person("John", 42, "green");
+var p2 = new person("Amy", 21, "red");
+
+document.write(p1.age); // Outputs 42
+document.write(p2.name); // Outputs "Amy"
+
