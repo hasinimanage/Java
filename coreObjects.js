903f0f52ac8fab79c7b21f5d971958323525b91a
unknown <hasinimanage@gmail.com>
2020-09-10 06:53:18 +0530

    creating an array

--- coreObjects.js
+++ coreObjects.js
@@ -2,3 +2,11 @@
 var courses = new Array("HTML", "CSS", "JS"); 
 var course = courses[0]; // HTML
 courses[1] = "C++"; //Changes the second element 
+
+
+//**creating an array */
+var courses = new Array();
+courses[0] = "HTML";
+courses[1] = "CSS";
+courses[2] = "JS";
+courses[3] = "C++";
